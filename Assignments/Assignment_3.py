ce0cde402aafb6b7bc76dffae4c664e6549f5eb5
manas baranwal <manas.baranwal13@gmail.com>
2025-04-20 00:46:17 +0530

    question 3

---
+++ Assignments/Assignment_3.py
@@ -0,0 +1,16 @@
+s=input("enter a string: ")
+v="aeiouAEIOU"
+vowel=0
+consonant=0
+for i in s:
+    if 'a'<=i<='z' or 'A'<=i<='Z':
+        if i in v:
+            vowel += 1
+        else:
+            consonant += 1
+
+print("Vowels=", vowel)
+print("Consonants=", consonant)
+
+
+
